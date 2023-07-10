292738cdd89a3513fab9686da52fc5ef86814205
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-07-10 08:14:28 +0300

    End The Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,24 @@ setInterval(() => {
   let random = bgArray[Math.floor(Math.random() * bgArray.length)];
   landing.style.backgroundImage = 'url("../../assets/image/' + random +'")';
 }, 3000);
+
+// scroll to top
+let scrollToTop = document.querySelector(".scroll-to-top");
+let btn = document.querySelector(".up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY >= 400) {
+    scrollToTop.style.display = "block";
+    scrollToTop.style.opactiy = "1";
+  } else {
+    scrollToTop.style.opactiy = "0";
+    scrollToTop.style.display = "none";
+  }
+});
+
+btn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
