1dbe8e346ca06dc08d712bb25d8df20ea2f703da
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-07-11 08:12:14 +0300

    solve some problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 // Random Background
 let landing = document.querySelector(".landing");
-let bgArray = ["../../assets/image/slider-01.jpg", "../../assets/image/slider-02.jpg", "../../assets/image/slider-03.jpg"];
+let bgArray = ["slider-01.jpg", "slider-02.jpg", "slider-03.jpg"];
 
 setInterval(() => {
   let random = bgArray[Math.floor(Math.random() * bgArray.length)];
-  landing.style.backgroundImage = `url(${random})`;
+  landing.style.backgroundImage = 'url("../../assets/image/'+ random +'")';
 }, 3000);
 // scroll to top
 let scrollToTop = document.querySelector(".scroll-to-top");
